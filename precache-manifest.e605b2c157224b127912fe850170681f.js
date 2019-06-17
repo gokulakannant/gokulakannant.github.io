4636ddc70e4743d281253cbbdb18d72b4e06e94a
gokulakannant <gokulakannanthangaraji@gmail.com>
2019-06-17 15:04:11 +0530

    Digital Resume

---
+++ precache-manifest.e605b2c157224b127912fe850170681f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c470ae04517bbb6f4a8149dc494723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ba9dc1f30db27f7d71",
+    "url": "/static/css/main.08167ab8.chunk.css"
+  },
+  {
+    "revision": "d7d9a3a18887e0bb3618",
+    "url": "/static/js/2.f69252ea.chunk.js"
+  },
+  {
+    "revision": "69ba9dc1f30db27f7d71",
+    "url": "/static/js/main.58b0211a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0a0225117250f9e2317ccfdc393638f",
+    "url": "/static/media/Gokulakannan.a0a02251.pdf"
+  },
+  {
+    "revision": "37136f10d090a0478b2d49b95ecea743",
+    "url": "/static/media/ProfileImage.37136f10.jpg"
+  }
+]);
