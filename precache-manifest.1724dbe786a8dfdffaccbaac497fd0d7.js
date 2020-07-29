6c7b3b503ceec6d7fe199fe73e361162386a6abd
gokulakannant <gokulakannanthangaraji@gmail.com>
2020-07-29 17:22:23 +0530

    New design with animations

---
+++ precache-manifest.1724dbe786a8dfdffaccbaac497fd0d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a65175f7270ea745126923d1341405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c643ebde4587bfa28119",
+    "url": "/static/css/main.741a494a.chunk.css"
+  },
+  {
+    "revision": "fb0b1208a4d2715d6e15",
+    "url": "/static/js/2.cd84957c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.cd84957c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c643ebde4587bfa28119",
+    "url": "/static/js/main.440a6e84.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  },
+  {
+    "revision": "727c11b71b82eb2ebdd25ca307ff60ef",
+    "url": "/static/media/Gokulakannan.727c11b7.pdf"
+  },
+  {
+    "revision": "37136f10d090a0478b2d49b95ecea743",
+    "url": "/static/media/ProfileImage.37136f10.jpg"
+  },
+  {
+    "revision": "c82b184bf7450e14adccb7b0d6117474",
+    "url": "/static/media/Quicksand-SemiBold.c82b184b.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e91756a0c316b2e3a1cc4e6628f8aa0c.js"
+  "/precache-manifest.1724dbe786a8dfdffaccbaac497fd0d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
